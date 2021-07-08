eabc2056789a630e89a1511acd5fc07b96b1d757
ahmadXtn <Ahmed.Bouazizi@issatmh.u-monastir.tn>
2021-07-08 23:30:52 +0200

    images and assets optimization : image-minimizer-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,8 @@ const PurgecssPlugin = require('purgecss-webpack-plugin');
 const terserJsPlugin = require('terser-webpack-plugin');
 
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
 
 const PATHS = {
 	src: path.join(__dirname, 'src')
@@ -56,6 +57,35 @@ module.exports = merge(common, {
 		new CssMinimizerPlugin(),
 		new CleanWebpackPlugin({
 			cleanOnceBeforeBuildPatterns: [],
+		}),
+		new ImageMinimizerPlugin({
+			minimizerOptions: {
+				// Lossless optimization with custom option
+				// Feel free to experiment with options for better result for you
+				plugins: [
+					["gifsicle", { interlaced: true }],
+					["jpegtran", { progressive: true }],
+					["optipng", { optimizationLevel: 5 }],
+					// Svgo configuration here https://github.com/svg/svgo#configuration
+					[
+						"svgo",
+						{
+							plugins: extendDefaultPlugins([
+								{
+									name: "removeViewBox",
+									active: false,
+								},
+								{
+									name: "addAttributesToSVGElement",
+									params: {
+										attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+									},
+								},
+							]),
+						},
+					],
+				],
+			},
 		}),
 	],
 
